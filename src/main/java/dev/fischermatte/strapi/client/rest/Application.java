06dcdf4b27bd4206003234d6335a82b0d80d1eb1
fischermatte <fischermatte@posteo.net>
2022-11-12 23:08:34 +0100

    refactor: reverted

--- src/main/java/dev/fischermatte/strapi/client/rest/Application.java
+++ src/main/java/dev/fischermatte/strapi/client/rest/Application.java
@@ -1,9 +1,14 @@
 package dev.fischermatte.strapi.client.rest;
 
+import dev.fischermatte.strapi.client.rest.api.EmailTemplateApi;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
 
 @Slf4j
 @SpringBootApplication
@@ -15,9 +20,38 @@ public class Application {
     @Value("${strapi.endpoint.url}")
     private String strapiEndpointUrl;
 
-
     public static void main(String[] args) {
         var ctx = SpringApplication.run(Application.class, args);
+        var emailTemplateApi = ctx.getBean(EmailTemplateApi.class);
+        var result = emailTemplateApi.getEmailTemplates(
+                null,
+                true,
+                0,
+                10,
+                0,
+                10,
+                null,
+                null,
+                null
+        );
+        result.subscribe(emailTemplateListResponse -> {
+            System.out.println(emailTemplateListResponse.toString());
+        }, throwable -> {
+            System.out.println(throwable.getMessage());
+        });
+    }
+
+    @Bean
+    public ApiClient apiClient() {
+        var apiClient = new ApiClient();
+        apiClient.setBearerToken(strapiApiToken);
+        apiClient.setBasePath(strapiEndpointUrl);
+        return apiClient;
+    }
+
+    @Bean
+    public EmailTemplateApi emailTemplateApi(ApiClient apiClient) {
+        return new EmailTemplateApi(apiClient);
     }
 
 }
